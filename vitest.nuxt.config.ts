53228ae8157bd5bdaae1914420d2beaa00b08176
Daniel Roe <daniel@roe.dev>
2024-12-17 16:38:52 +0000

    test: update snapshots

--- vitest.nuxt.config.ts
+++ vitest.nuxt.config.ts
@@ -1,5 +1,9 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default defineVitestConfig({
   test: {
     dir: './test/nuxt',
